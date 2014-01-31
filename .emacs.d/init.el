172a593ac9606f8168505f673c42dc30922d517d
azalea <mazhaorong@gmail.com>
2014-01-31 11:13:33 -0500

    Added .emacs.d/init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,148 @@
+;; Requisites: Emacs >= 24
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) 
+(package-refresh-contents)
+
+;; (defun install-if-needed (package)
+;;   (unless (package-installed-p package)
+;;     (package-install package)))
+
+;; ;; make more packages available with the package installer
+;; (setq to-install
+;;       '(python-mode magit yasnippet jedi auto-complete autopair find-file-in-repository org))
+
+;; (mapc 'install-if-needed to-install)
+
+
+(require 'magit)
+(global-set-key "\C-xg" 'magit-status)
+
+(require 'auto-complete)
+(require 'autopair)
+(require 'flymake)
+(require 'yasnippet)
+
+(global-set-key [f7] 'find-file-in-repository)
+
+
+; auto-complete mode extra settings
+(setq
+ ac-auto-start 2
+ ac-override-local-map nil
+ ac-use-menu-map t
+ ac-candidate-limit 20)
+
+;; ;; Python mode settings
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(setq py-electric-colon-active t)
+(add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'python-mode-hook 'yas-minor-mode)
+
+;; ;; Jedi settings
+(require 'jedi)
+;; It's also required to run "pip install --user jedi" and "pip
+;; install --user epc" to get the Python side of the library work
+;; correctly.
+;; With the same interpreter you're using.
+
+;; if you need to change your python intepreter, if you want to change it
+;; (setq jedi:server-command
+;;       '("python2" "/home/andrea/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (jedi:setup)
+	    (jedi:ac-setup)
+            (local-set-key "\C-cd" 'jedi:show-doc)
+            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            (local-set-key (kbd "M-.") 'jedi:goto-definition)))
+
+;; Flymake settings for Python
+(defun flymake-python-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "epylint" (list local-file))))
+
+(defun flymake-activate ()
+  "Activates flymake when real buffer and you have write access"
+  (if (and
+       (buffer-file-name)
+       (file-writable-p buffer-file-name))
+      (progn
+        (flymake-mode t)
+        ;; this is necessary since there is no flymake-mode-hook...
+        (local-set-key (kbd "C-c n") 'flymake-goto-next-error)
+        (local-set-key (kbd "C-c p") 'flymake-goto-prev-error))))
+
+(defun ca-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'ca-flymake-show-help)
+
+
+(add-to-list 'flymake-allowed-file-name-masks
+             '("\\.py\\'" flymake-python-init))
+
+(add-hook 'python-mode-hook 'flymake-activate)
+(add-hook 'python-mode-hook 'auto-complete-mode)
+
+(ido-mode t)
+
+;; -------------------- extra nice things --------------------
+;; use shift to move around windows
+(windmove-default-keybindings 'shift)
+(show-paren-mode t)
+ ; Turn beep off
+(setq visible-bell nil)
+
+;; Orgmode settings
+; Must have org-mode loaded before we can configure org-babel
+(require 'org-install)
+
+; Some initial langauges we want org-babel to support
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (sh . t)
+   (python . t)
+   (R . t)
+   (ruby . t)
+   (ditaa . t)
+   (dot . t)
+   (octave . t)
+   (sqlite . t)
+   (perl . t)
+   ))
+
+; Add short cut keys for the org-agenda
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+
+
+
+; Source: http://www.emacswiki.org/emacs/CommentingCode
+;; Original idea from
+;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+  If no region is selected and current line is not blank and we are not at the end of the line,
+  then comment current line.
+  Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+    (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'comment-dwim-line)
