087f784ac664b3e1335af8d79640076edc635901
azalea <mazhaorong@gmail.com>
2015-12-10 09:45:33 -0500

    Set default-directory. Load ESS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,6 +51,10 @@
 
 (require 'yasnippet)
 
+(let ((default-directory  "~/.emacs.d/"))
+    (normal-top-level-add-subdirs-to-load-path))
+(load "ess-site")
+
 ; Source: http://www.emacswiki.org/emacs/CommentingCode
 ;; Original idea from
 ;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
@@ -72,7 +76,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
- '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))))
+ '(custom-safe-themes
+   (quote
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
